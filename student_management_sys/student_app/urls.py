f7fa17d65fa3c32d7d48b18a12a3d2634348644e
santwan <santwanpathak1@gmail.com>
2024-04-24 14:22:07 +0530

    Added media, static , templates folder

--- student_management_sys/student_app/urls.py
+++ student_management_sys/student_app/urls.py
@@ -5,13 +5,14 @@ from . import views
 from .import HodViews, StaffViews, StudentViews 
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls), 
     path('', views.home, name="home"), 
-    path('contact', views.contact, name="contact"), 
-    path('login', views.loginUser, name="login"), 
-    path('logout_user', views.logout_user, name="logout_user"), 
-    path('registration', views.registration, name="registration"), 
-    path('doLogin', views.doLogin, name="doLogin"), 
-    path('doRegistration', views.doRegistration, name="doRegistration"), 
+    # path('contact', views.contact, name="contact"), 
+    # path('login', views.loginUser, name="login"), 
+    # path('logout_user', views.logout_user, name="logout_user"), 
+    # path('registration', views.registration, name="registration"), 
+    # path('doLogin', views.doLogin, name="doLogin"), 
+    # path('doRegistration', views.doRegistration, name="doRegistration"), 
 ]
 
--- student_management_sys/student_app/views.py
+++ student_management_sys/student_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , HttpResponse , redirect , HttpResponseRedirect
 
 
-
-
-
 #Create your views here.
+
+def home(request): 
+    return render(request, 'home.html') 
